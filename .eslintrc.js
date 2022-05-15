f69c4a1d973f0d30788a208ca252af87d1adeb9b
Jankku <29043938+Jankku@users.noreply.github.com>
2022-05-15 10:38:48 +0300

    chore: project setup

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'react-app',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  plugins: ['react', 'react-hooks', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-undef': 'off',
+    'no-unused-vars': 'off',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
